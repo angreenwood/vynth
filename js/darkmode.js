39cb93627799fc7258ce8da5e51039e6f9fb1f1c
angreenwood <ghezts@gmail.com>
2020-02-01 10:44:50 -0500

    daynight

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,5 +1,7 @@
 const lightMode = document.querySelector('.sun');
 const darkMode = document.querySelector('.moon');
+const nightMode = document.querySelector('.nightmode');
+const dayMode = document.querySelector('.daymode');
 
 const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
 
@@ -7,12 +9,16 @@ lightMode.onclick = function(){
   document.documentElement.setAttribute('data-theme', 'dark');
   lightMode.style = "display: none";
   darkMode.style = "display: block";
+  nightMode.style = "display: none";
+  dayMode.style = "display: block";
   localStorage.setItem('theme', 'dark');
 }
 
 darkMode.onclick = function(){
   document.documentElement.setAttribute('data-theme', 'light');
   lightMode.style = "display: block";
+  dayMode.style = "display: none";
+  nightMode.style = "display:block";
   darkMode.style = "display: none";
   localStorage.setItem('theme', 'light');
   
